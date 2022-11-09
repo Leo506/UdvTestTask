ee183f7c85ee55c4d38c60f967f0efc60b36bda5
Artem Levin <leoartemlev@yandex.ru>
2022-11-09 20:24:31 +0500

    scaffold db context

---
+++ UdvTestTask/UdvTestTask/Data/LettersCountDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdvTestTask.Models;
+
+namespace UdvTestTask.Data;
+
+public partial class LettersCountDbContext : DbContext
+{
+    public LettersCountDbContext()
+    {
+    }
+
+    public LettersCountDbContext(DbContextOptions<LettersCountDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<LettersCount> LettersCounts { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LettersCount>(entity =>
+        {
+            entity.ToTable("LettersCount");
+
+            entity.Property(e => e.LettersData).HasColumnType("jsonb");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ UdvTestTask/UdvTestTask/Models/LettersCount.cs
@@ -0,0 +1,7 @@
+﻿namespace UdvTestTask.Models;
+
+public partial class LettersCount
+{
+    public int Id { get; set; }
+    public string LettersData { get; set; } = null!;
+}
